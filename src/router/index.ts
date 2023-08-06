550f758a2e590c319d299eff0f0daa280b95119d
ChangThai <thadametphokhin@gmail.com>
2023-08-06 15:51:59 +0700

    add file ads.txt

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Content from '../App.vue'
-
+import ads from './ads.txt'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/about',
-      name: 'about',
+      path: '/ads',
+      name: 'ads',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => (null)
+      component: () => (ads)
     }
   ]
 })
