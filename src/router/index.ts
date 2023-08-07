3e84c03fca99319a7c591de3c27b854ec99204d8
ChangThai <thadametphokhin@gmail.com>
2023-08-07 17:03:33 +0700

    change edit code in index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Content from '../App.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/asd',
+      path: '/',
       name: 'asd',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
@@ -13,5 +12,5 @@ const router = createRouter({
     }
   ]
 })
-
+export default router
 
