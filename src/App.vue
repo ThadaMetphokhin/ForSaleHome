e56a07e9979d68a909fdcd74ded6501dd43e71ff
ChangThai <thadametphokhin@gmail.com>
2023-08-07 17:11:35 +0700

    test 4

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import Content from './views/Content.vue'
-import { Analytics } from '@vercel/analytics/react';
 
 
 </script>
 
 <template>
   <Content ></Content>
-  <Analytics></Analytics>
 </template>
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Kodchasan:wght@300&display=swap');
